a078a8d8d67b17092584a0afe1a7b3e09fd2354b
BeevMan <52551233+BeevMan@users.noreply.github.com>
2019-11-20 03:47:27 -0600

    Update hbKeyPgNameLister.js

--- hbKeyPgNameLister.js
+++ hbKeyPgNameLister.js
@@ -1,10 +1,9 @@
-
-
 function findNames (){
 	let myGames = [];
 	let lastPage = document.getElementsByClassName("js-jump-to-page jump-to-page").length-2; // how to find the length of the pages tab, document.getElementsByClassName("js-jump-to-page jump-to-page")[8].innerText; the second to last button for key page selection always seems to have the page total. the last button is usually the button to click to next page?
 	let pages = document.getElementsByClassName("js-jump-to-page jump-to-page")[lastPage].innerText; // removes the text from the selected element using lastPage as the index
 	let pageCount = Number(pages); // converts pages to a number
+	
 	for (let j = 0; j< pageCount;j++){ // loops through hb's key pages 
 		for (let i = 0; i < document.querySelectorAll("h4").length; i++) { // loops through all "h4" 's. <h4> is where the game names are stored
 		    myGames.push(document.getElementsByTagName('h4')[i].innerHTML); // pushes the games name to the array
@@ -12,9 +11,8 @@ function findNames (){
 		if (j < pageCount -1){
 			document.getElementsByClassName("hb hb-chevron-right")[0].click(); // navigates one page to the right on humbles key page
 	    }
+	}
 	
-    }
-
     let games = myGames.toString(); // converts myGames to a string
     let laidout = games.replace(/,/g, '\n'); // replaces "," with a linebreak, using this method removes "," from game names like The Haunted Island, a Frog Detective Game making them not found in my app
     let test = laidout.replace(/\n /g, ', '); // re adds "," & " " to game names that previously had it removed
