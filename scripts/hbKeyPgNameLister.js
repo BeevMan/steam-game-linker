dcb7a6b2afed7441cc77a64a9151924d33bd640b
BeevMan <52551233+BeevMan@users.noreply.github.com>
2019-12-20 06:25:45 -0600

    Add files via upload

--- scripts/hbKeyPgNameLister.js
+++ scripts/hbKeyPgNameLister.js
@@ -1,26 +1,30 @@
 
 
 function findNames (){
-	let arrMyGames = [];
-	let elJumpTo = document.getElementsByClassName("js-jump-to-page jump-to-page"); // stores the js-jump-to-page to a local variable
-	let intLastPage = elJumpTo.length-2; // how to find the length of the pages tab, document.getElementsByClassName("js-jump-to-page jump-to-page")[8].innerText; the second to last button for key page selection always seems to have the page total. the last button is usually the button to click to next page?
-	let intPageCount = Number(elJumpTo[intLastPage].innerText) || 0; // converts pages to a number
-	
+	let strGames = "";
+	let elJumpTo = document.getElementsByClassName("js-jump-to-page jump-to-page");
+	let intLastPage = elJumpTo.length-2;
+	let intPageCount = Number(elJumpTo[intLastPage].innerText) || 0;
+	console.log("pre loop");
 	for (let j = 0; j< intPageCount;j++){ // loops through hb's key pages 
-	    let elH4 = document.querySelectorAll("h4");
+	    let elH4 = document.querySelectorAll("h4"); // locally stores the "h4" elements
 		for (let i = 0; i < elH4.length; i++) { // loops through all "h4" 's. <h4> is where the game names are stored
-		    arrMyGames.push(elH4[i].innerHTML); // pushes the games name to the array
+		    strGames += elH4[i].innerHTML + "\n"; // pushes the games name to the array
 		}
 		if (j < intPageCount -1){
 			document.getElementsByClassName("hb hb-chevron-right")[0].click(); // navigates one page to the right on humbles key page
 	    }
 	}
-	
-    let strGames = arrMyGames.toString(); // converts myGames to a string
-    strGames = strGames.replace(/,/g, '\n'); // replaces "," with a linebreak, using this method removes "," from game names like The Haunted Island, a Frog Detective Game making them not found in my app
-    strGames = strGames.replace(/\n /g, ', '); // re adds "," & " " to game names that previously had it removed
     
-	copy(strGames); // copies the string to users clipboard ALREADY A FUNCTION on HumbleBundle
+	function textToClipboard (text) {
+    let dummy = document.createElement("textarea");
+    document.body.appendChild(dummy);
+    dummy.value = text;
+    dummy.select();
+    document.execCommand("copy");
+    document.body.removeChild(dummy);
+    }
+	textToClipboard(strGames); // copies the string to users clipboard
     
 	alert("Game names copied to clipboard!!");
 }
