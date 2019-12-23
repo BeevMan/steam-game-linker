7b942d97755e3457024956d6573648a24a5ddd95
BeevMan <52551233+BeevMan@users.noreply.github.com>
2019-12-22 21:38:34 -0600

    Add files via upload

--- scripts/hbKeyPgNameLister.js
+++ scripts/hbKeyPgNameLister.js
@@ -1,32 +1,33 @@
 
 
-function findNames (){
+function copyList(){
 	let strGames = "";
 	let elJumpTo = document.getElementsByClassName("js-jump-to-page jump-to-page");
 	let intLastPage = elJumpTo.length-2;
-	let intPageCount = Number(elJumpTo[intLastPage].innerText) || 0;
-	console.log("pre loop");
-	for (let j = 0; j< intPageCount;j++){ // loops through hb's key pages 
-	    let elH4 = document.querySelectorAll("h4"); // locally stores the "h4" elements
-		for (let i = 0; i < elH4.length; i++) { // loops through all "h4" 's. <h4> is where the game names are stored
-		    strGames += elH4[i].innerHTML + "\n"; // pushes the games name to the array
+	let intPageCount = 1;
+	if(intLastPage > 0){
+		intPageCount = Number(elJumpTo[intLastPage].innerText);
+		if(elJumpTo[1].innerText === "1"){
+			elJumpTo[1].click();
+		}
+	}
+	for (let j = 0; j< intPageCount;j++){
+		let elH4 = document.querySelectorAll("h4");
+		for (let i = 0; i < elH4.length; i++){
+			strGames += elH4[i].innerHTML + "\n";
 		}
 		if (j < intPageCount -1){
-			document.getElementsByClassName("hb hb-chevron-right")[0].click(); // navigates one page to the right on humbles key page
-	    }
-	}
-    
-	function textToClipboard (text) {
-    let dummy = document.createElement("textarea");
-    document.body.appendChild(dummy);
-    dummy.value = text;
-    dummy.select();
-    document.execCommand("copy");
-    document.body.removeChild(dummy);
-    }
-	textToClipboard(strGames); // copies the string to users clipboard
-    
-	alert("Game names copied to clipboard!!");
-}
-findNames();
-
+			document.getElementsByClassName("hb hb-chevron-right")[0].click();
+		}
+	}	
+	let dummy = document.createElement("textarea");
+	document.body.appendChild(dummy);
+	dummy.value = strGames;
+	dummy.select();
+	document.execCommand("copy");
+	document.body.removeChild(dummy);
+};
+let btn = document.createElement("label");
+btn.innerHTML = '<button id ="copier" style = "padding: 0.1em .2em; background-color: red; color: white; text-align: center" >list to clipboard</button>';
+document.getElementsByClassName("sort")[0].appendChild(btn);
+document.getElementById("copier").addEventListener("click", copyList);
