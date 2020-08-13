83ecdd826653f6f76a1bb2727aa806823cba5dc4
BeevMan <52551233+BeevMan@users.noreply.github.com>
2020-08-12 20:30:45 -0500

    Update hbKeyPgNameLister.js

--- scripts/hbKeyPgNameLister.js
+++ scripts/hbKeyPgNameLister.js
@@ -1,11 +1,10 @@
-
 function copyList(){
 	let strGames = "";
 	let elJumpTo = document.getElementsByClassName("js-jump-to-page jump-to-page");
 	let intLastPage = elJumpTo.length-2;
 	let intPageCount = 1;
-	let monthlies = []; // stores the URLs for monthly choices
-	let monthlyInfo = []; // stores name of the monthly and how many choices remaining.  Should have same length as monthlies var
+	let monthlies = [];
+	let monthlyInfo = [];
 	if(intLastPage > 0){
 		intPageCount = Number(elJumpTo[intLastPage].innerText);
 		if(elJumpTo[1].innerText === "1"){
@@ -16,13 +15,13 @@ function copyList(){
 		let elH4 = document.querySelectorAll("h4");
 		let elChoiceUrls = document.getElementsByClassName("choice-button");
 		let descriptions = document.getElementsByClassName("game-name");
-		if (elChoiceUrls.length > 0 ){ // itterates over the monthly choices URL's and pushes them into monthlies var
+		if (elChoiceUrls.length > 0 ){ 
 			for (let i = 0; i < elChoiceUrls.length; i++){
 				monthlies.push(elChoiceUrls[i].href);
 			};
 		};
 		for (let i = 0; i < elH4.length; i++){
-			if(elH4[i].innerHTML.endsWith("Humble Choice")){ // if its a choice, stores name of the monthly choice and how many choices remaining
+			if(elH4[i].innerHTML.endsWith("Humble Choice")){
 				let tempInfo = descriptions[i+1].innerText.split("\n",2);
 				tempInfo[1] = tempInfo[1].slice(9);
 				tempInfo = tempInfo[0] + "\n" + tempInfo[1];
@@ -55,14 +54,11 @@ function clipboard(toCopy){
 	
 	if (navigator.clipboard){
 		navigator.clipboard.writeText(toCopy).then(function() {
-			//clipboard successfully set
 			alert("Game list copied to clipboard!");
 		}, function() {
-			// clipboard write failed
 			alert("Could not copy list to clipboard.");
 		});
 	} else {
-		// this way will copy my unredeemed list fine (121 lines) but will not copy my entire list (872 lines)
 		let dummy = document.createElement("textarea");
 		document.body.appendChild(dummy);
 		dummy.value = toCopy;
@@ -76,7 +72,6 @@ function clipboard(toCopy){
 
 
 async function logInOrder(urls,strGames,monthlyInfo) {
-  // fetch all the URLs in parallel
   const textPromises = urls.map(async url => {
     const response = await fetch(url);
     return response.text();
@@ -84,30 +79,38 @@ async function logInOrder(urls,strGames,monthlyInfo) {
   
   
   let loopInt = 0;
-  // log them in sequence
   for (const textPromise of textPromises) {
 	let parser = new DOMParser();
 	let doc = parser.parseFromString(await textPromise, 'text/html');
 	
 	
-	// Get the monthly data
-	let monthlyData = JSON.parse(doc.getElementById("webpack-monthly-product-data").innerText).contentChoiceOptions;
-	let choiceData = monthlyData.contentChoiceData.initial.content_choices;
+	const monthlyData = JSON.parse(doc.getElementById("webpack-monthly-product-data").innerText).contentChoiceOptions;
+		let choicePlan = Object.keys(monthlyData.contentChoiceData)[0];
+		if (choicePlan.startsWith('initial') === false) {
+			const ccdKeys = Object.keys(monthlyData.contentChoiceData);
+			for (let i = 1; i < ccdKeys.length; i++ ){ 
+				if (ccdKeys[i].startsWith('initial')){
+					choicePlan = ccdKeys[i];
+					break
+				}
+			};
+		}
+		let choiceData = monthlyData.contentChoiceData[choicePlan].content_choices;
 	
 	if (monthlyData.hasOwnProperty("contentChoicesMade")) {
-		let choicesMade = monthlyData.contentChoicesMade.initial.choices_made;
+		let choicesMade = monthlyData.contentChoicesMade[choicePlan].choices_made;
 		
-		for (let i = 0; i < choicesMade.length; i++){ //deletes choices made from choiceData
+		for (let i = 0; i < choicesMade.length; i++){
 			if(choiceData.hasOwnProperty(choicesMade[i])){
 				delete choiceData[choicesMade[i]];
 			}
 		};
 	}
-	strGames += monthlyInfo[loopInt] + "\n"; // adds the current monthly info i.e MARCH 2020 HUMBLE CHOICE 4 choices remaining
+	strGames += monthlyInfo[loopInt] + "\n";
 	let choiceKeys = Object.keys(choiceData);
 
 	for (let i = 0; i < choiceKeys.length; i++){
-		let curKey = choiceKeys[i]; //I know choiceData has human name and also steam app id which i could use to link the game to steam website
+		let curKey = choiceKeys[i];
 		strGames += choiceData[curKey].title + "\n";
 	};
 	loopInt++;
